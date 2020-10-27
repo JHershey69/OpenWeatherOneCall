355e0eb794ec00d8580a7303b5d9057db52c7854
Jon Fuge <jon.fuge@gmail.com>
2020-10-27 18:53:32 +0100

    Added minutely, hourly and alerts; reorganised code to allow Options to be switched on/off although not in a great way with defines.
    Signed-off-by: Jon Fuge <jon.fuge@gmail.com>

--- src/OpenWeatherOneCall.cpp
+++ src/OpenWeatherOneCall.cpp
@@ -6,6 +6,8 @@
    Open Weather Seven Day Forecast Library
    Seven Day and Current Weather Conditions
    Viva La Resistance
+   
+   Minutely, Hourly and alerts added - Jon Fuge
 */
 
 #include "Arduino.h"            // Basic Arduino Library
@@ -29,7 +31,6 @@ OpenWeatherOneCall::OpenWeatherOneCall()
 
 
 String short_names[7] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-
 
 int OpenWeatherOneCall::getCoordinates(String googleKey)
 {
@@ -199,64 +200,173 @@ int OpenWeatherOneCall::parseWeather(char* DKEY, char* GKEY, float SEEK_LATITUDE
 
     deserializeJson(doc, http.getString()); // <<---------------- - Streams JSON
 
+#ifdef CURRENT
     JsonObject currently = doc["current"];
-    current.dayTime = currently["dt"]; // 1586781931
-    current.temperature = currently["temp"]; // 287.59
-    current.apparentTemperature = currently["feels_like"]; // 281.42
+    current.dt = currently["dt"]; // 1586781931
+	current.sunrise = currently["sunrise"]; // 1586773262
+	current.sunset = currently["sunset"]; // 1586820773
+    current.temp = currently["temp"]; // 287.59
+    current.feels_like = currently["feels_like"]; // 281.42
     current.pressure = currently["pressure"]; // 1011
     current.humidity = currently["humidity"]; // 93
-    current.dewPoint = currently["dew_point"]; // 286.47
-    current.uvIndex = currently["uvi"]; // 6.31
-    current.cloudCover = currently["clouds"]; // 90
+    current.dew_point = currently["dew_point"]; // 286.47
+    current.clouds = currently["clouds"]; // 90
+    current.uvi = currently["uvi"]; // 6.31
     current.visibility = currently["visibility"]; // 8047
+    current.wind_speed = currently["wind_speed"]; // 10.3
+    current.wind_gust = currently["wind_gust"]; // 14.4
+    current.wind_deg = currently["wind_deg"]; // 170
+    current.rain_1h = currently["rain"]["1h"]; // 14.4
+    current.snow_1h = currently["snow"]["1h"]; // 14.4
+    current.id = currently["weather"][0]["id"];  // 500
+    current.main = currently["weather"][0]["main"];  // "rain"
+    current.description = currently["weather"][0]["description"];  // "moderate rain"
+	#ifndef DARKSKY
+    current.icon = currently["weather"][0]["icon"];
+	#endif
+	#ifdef DARKSKY
+	// Legacy DarkSky compatibility
+    current.dayTime = currently["dt"]; // 1586781931
+	current.sunriseTime = currently["sunrise"]; // 1586773262
+	current.sunsetTime = currently["sunset"]; // 1586820773
+    current.temperature = currently["temp"]; // 287.59
+    current.apparentTemperature = currently["feels_like"]; // 281.42
+    current.cloudCover = currently["clouds"]; // 90
+    current.uvIndex = currently["uvi"]; // 6.31
     current.windSpeed = currently["wind_speed"]; // 10.3
-    current.windBearing = currently["wind_deg"]; // 170
     current.windGust = currently["wind_gust"]; // 14.4
+    current.windBearing = currently["wind_deg"]; // 170
+    current.summary = currently["weather"][0]["description"];  // "moderate rain"
     current.icon = currently["weather"][0]["main"];
+	#endif
+#endif
 
+#ifdef MINUTELY
+// JsonObject minutely = minutely;
+    JsonArray everyminute = doc["minutely"];
 
+//===================================
+// FOR loop to fill the minutely
+//===================================
+
+    for (int x = 0; x < (sizeof(minutely) / sizeof(minutely[0])); x++)
+    {
+        minutely[x].dt = everyminute[x]["dt"]; // 1586793600
+        minutely[x].precipitation = everyminute[x]["precipitation"]; // 1586793600
+	}
+//*************************END OF FOR LOOP
+//****************************************
+#endif
+
+#ifdef HOURLY
+// JsonObject hourly = hourly;
+    JsonArray everyhour = doc["hourly"];
+
+//===================================
+// FOR loop to fill the hourly
+//===================================
+
+    for (int x = 0; x < (sizeof(hourly) / sizeof(hourly[0])); x++)
+    {
+        hourly[x].dt = everyhour[x]["dt"]; // 1586793600
+        hourly[x].temp = everyhour[x]["temp"]; // 13.45
+        hourly[x].feels_like = everyhour[x]["feels_like"]; // 13.45
+        hourly[x].pressure = everyhour[x]["pressure"]; // 13.45
+        hourly[x].humidity = everyhour[x]["humidity"]; // 13.45
+        hourly[x].dew_point = everyhour[x]["dew_point"]; // 13.45
+        hourly[x].clouds = everyhour[x]["clouds"]; // 13.45
+        hourly[x].visibility = everyhour[x]["visibility"]; // 13.45
+        hourly[x].wind_speed = everyhour[x]["wind_speed"]; // 13.45
+        hourly[x].wind_gust = everyhour[x]["wind_gust"]; // 13.45
+        hourly[x].wind_deg = everyhour[x]["wind_deg"]; // 13.45
+        hourly[x].pop = everyhour[x]["pop"]; // 13.45
+        hourly[x].rain_1h = everyhour[x]["rain"]["1h"]; // 13.45
+        hourly[x].snow_1h = everyhour[x]["snow"]["1h"]; // 13.45
+        hourly[x].wind_deg = everyhour[x]["wind_deg"]; // 13.45
+        hourly[x].id = everyhour[x]["weather"][0]["id"]; // 500
+        hourly[x].main = everyhour[x]["weather"][0]["main"]; // "rain"
+        hourly[x].description = everyhour[x]["weather"][0]["description"]; // "moderate rain"
+        hourly[x].icon = everyhour[x]["weather"][0]["icon"]; // "10d"
+	}
+//*************************END OF FOR LOOP
+//****************************************
+#endif
+
+#ifdef DAILY
+// JsonObject daily_0 = daily[0];
+    JsonArray daily = doc["daily"];
+	
 //===================================
 // FOR loop to fill the 8 day struct
 //===================================
 
-
-    JsonArray daily = doc["daily"];
-// JsonObject daily_0 = daily[0];
-
-
-    for (int x = 0; x < (sizeof(forecast) / sizeof(forecast[0])) - 1; x++)
+    for (int x = 0; x < (sizeof(forecast) / sizeof(forecast[0])); x++)
     {
-        forecast[x].dayTime = daily[x]["dt"]; // 1586793600
-        forecast[x].sunriseTime = daily[x]["sunrise"]; // 1586773262
-        forecast[x].sunsetTime = daily[x]["sunset"]; // 1586820773
+        forecast[x].dt = daily[x]["dt"]; // 1586793600
+        forecast[x].sunrise = daily[x]["sunrise"]; // 1586773262
+        forecast[x].sunset = daily[x]["sunset"]; // 1586820773
 
-        forecast[x].temperatureHigh = daily[x]["temp"]["day"]; // 288.74
-        forecast[x].temperatureMin = daily[x]["temp"]["min"]; // 286.56
-        forecast[x].temperatureMax = daily[x]["temp"]["max"]; // 293.23
-        forecast[x].temperatureLow = daily[x]["temp"]["night"]; // 286.56
+        forecast[x].temp_morn = daily[x]["temp"]["morn"]; // 288.74
+        forecast[x].temp_day = daily[x]["temp"]["day"]; // 288.74
+        forecast[x].temp_eve = daily[x]["temp"]["eve"]; // 288.74
+        forecast[x].temp_night = daily[x]["temp"]["night"]; // 286.56
+        forecast[x].temp_min = daily[x]["temp"]["min"]; // 286.56
+        forecast[x].temp_max = daily[x]["temp"]["max"]; // 293.23
 
-        forecast[x].apparentTemperatureHigh = daily[x]["feels_like"]["day"]; // 280.11
-        forecast[x].apparentTemperatureLow = daily[x]["feels_like"]["night"]; // 280.29
+        forecast[x].feels_like_morn = daily[x]["feels_like"]["morn"]; // 280.11
+        forecast[x].feels_like_day = daily[x]["feels_like"]["day"]; // 280.11
+        forecast[x].feels_like_eve = daily[x]["feels_like"]["eve"]; // 280.11
+        forecast[x].feels_like_night = daily[x]["feels_like"]["night"]; // 280.29
 
         forecast[x].pressure = daily[x]["pressure"]; // 1006
         forecast[x].humidity = daily[x]["humidity"]; // 91
+        forecast[x].dew_point = daily[x]["dew_point"]; // 287.28
+        forecast[x].wind_speed = daily[x]["wind_speed"]; // 14.2
+        forecast[x].wind_gust = daily[x]["wind_gust"]; // 14.2
+        forecast[x].wind_deg = daily[x]["wind_deg"]; // 180
+        forecast[x].clouds = daily[x]["clouds"]; // 95
+        forecast[x].uvi = daily[x]["uvi"]; // 6.31
+        forecast[x].visibility = daily[x]["visibility"]; // 10000
+        forecast[x].pop = daily[x]["pop"]; // 0.57
+        forecast[x].rain = daily[x]["rain"]; // 23.31
+        forecast[x].snow = daily[x]["snow"]; // 23.31
+        forecast[x].id = daily[x]["weather"][0]["id"];  // 500
+        forecast[x].main = daily[x]["weather"][0]["main"];  // "rain"
+        forecast[x].description = daily[x]["weather"][0]["description"];  // "moderate rain"
+        forecast[x].icon = daily[x]["weather"][0]["icon"]; // "10d"
+		#ifdef DARKSKY
+		// Legacy DarkSky compatibility
+        forecast[x].dayTime = daily[x]["dt"]; // 1586793600
+        forecast[x].sunriseTime = daily[x]["sunrise"]; // 1586773262
+        forecast[x].sunsetTime = daily[x]["sunset"]; // 1586820773
+        forecast[x].temperatureHigh = daily[x]["temp"]["day"]; // 288.74
+        forecast[x].temperatureLow = daily[x]["temp"]["night"]; // 286.56
+        forecast[x].temperatureMin = daily[x]["temp"]["min"]; // 286.56
+        forecast[x].temperatureMax = daily[x]["temp"]["max"]; // 293.23
+        forecast[x].apparentTemperatureHigh = daily[x]["feels_like"]["day"]; // 280.11
+        forecast[x].apparentTemperatureLow = daily[x]["feels_like"]["night"]; // 280.29
         forecast[x].dewPoint = daily[x]["dew_point"]; // 287.28
         forecast[x].windSpeed = daily[x]["wind_speed"]; // 14.2
         forecast[x].windBearing = daily[x]["wind_deg"]; // 180
-
-        forecast[x].precipType = daily[x]["weather"][0]["main"]; // "Rain"
-        forecast[x].icon = daily[x]["weather"][0]["icon"]; // "10d"
-
         forecast[x].cloudCover = daily[x]["clouds"]; // 95
-        forecast[x].precipIntensity = daily[x]["rain"]; // 23.31
         forecast[x].uvIndex = daily[x]["uvi"]; // 6.31
+        forecast[x].precipType = daily[x]["weather"][0]["main"]; // "Rain"
+        forecast[x].precipIntensity = daily[x]["rain"]; // 23.31
+        forecast[x].summary = daily[x]["weather"][0]["description"];  // "moderate rain"
+		#endif
     }
-
-
 //*************************END OF FOR LOOP
 //****************************************
+#endif
 
-
+#ifdef ALERTS
+    JsonObject alert = doc["alerts"];
+    alerts.sender_name = alert["sender_name"]; // 1586781931
+    alerts.event = alert["event"]; // 1586781931
+    alerts.start = alert["start"]; // 1586781931
+    alerts.end = alert["end"]; // 1586781931
+    alerts.description = alert["description"]; // 1586781931
+#endif
 
     http.end();
     return 0;
