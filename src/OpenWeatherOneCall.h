cc014ae18df17acf08cc59af1e9f975be7fa4f02
Jessica Hershey <60907719+JHershey69@users.noreply.github.com>
2020-12-15 22:12:57 -0500

    Update OpenWeatherOneCall.h

--- src/OpenWeatherOneCall.h
+++ src/OpenWeatherOneCall.h
@@ -58,7 +58,7 @@ public:
     int setExcl(int _EXCL);
     int setUnits(int _UNIT);
     int setHistory(int _HIS);
-    char* getErrorMsg(int errorMsg);
+    char* getErrorMsgs(int errorMsg);
 
     //Legacy Method
     int parseWeather(char* DKEY, char* GKEY, float SEEK_LATITUDE, float SEEK_LONGITUDE, bool SET_UNITS, int CITY_ID, int API_EXCLUDES, int GET_HISTORY);
