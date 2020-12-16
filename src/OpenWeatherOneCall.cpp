045eb90fc020c342f997da2fd0daf32eb3fb61a9
Jessica Hershey <60907719+JHershey69@users.noreply.github.com>
2020-12-15 22:07:40 -0500

    fixed a typo

--- src/OpenWeatherOneCall.cpp
+++ src/OpenWeatherOneCall.cpp
@@ -988,7 +988,7 @@ void OpenWeatherOneCall::freeMinuteMem(void)
         }
 }
 
-char* OpenWeatherOneCall::getErrorMsgs(int errorMsg){
+char* OpenWeatherOneCall::getErrorMsg(int errorMsg){
 strcpy_P(buffer, (char*)pgm_read_dword(&(errorMsgs[errorMsg])));
 return buffer;
 }
